fd5e098f448d7210e043493f1dc01b5e00f28d02
maddie havraniak <havranim@email.sc.edu>
2021-10-13 21:14:14 -0400

    adding comments pattern

--- InputHandler.java
+++ InputHandler.java
@@ -11,28 +11,29 @@ public class InputHandler {
    private Command[] IDontKnowCommand;
    private Command[] InArmyCommand;
    private Command[] EverywhereCommand;
-
+//assign each instance to its correlating number
     public InputHandler(MilitaryCadence cadence) {
         IDontKnowCommand = new Command[0];
         InArmyCommand = new Command[1];
         EverywhereCommand = new Command[2]; 
     }
-
+//check to see if it has a valid index 
     public int playCadence(int num) {
         if(num == 0)
         {
-            return 0; 
+            IDontKnowCommand[0].execute(); //calling execute from Military Cadence
         }
         if(num == 1) {
-            return 1;
+            InArmyCommand[1].execute();  //calling execute from Military Cadence
         }
         if(num == 2) {
-            return 2;
+            EverywhereCommand[2].execute();//calling execute from Military Cadence
         }
         else {
             return -1;
         }
-        
+        return 0; 
     }
+   
 
 }
