f5a8ef45a83cc0a8f599e1d89addbee76e9c8285
maddie havraniak <havranim@email.sc.edu>
2021-10-13 21:05:52 -0400

    adding changes to input handler

--- InputHandler.java
+++ InputHandler.java
@@ -6,14 +6,32 @@
 import java.util.*;
 public class InputHandler {
 
+    //create an instance of each command class in an arraylist
     private ArrayList<Command> commands;
+   private Command[] IDontKnowCommand;
+   private Command[] InArmyCommand;
+   private Command[] EverywhereCommand;
 
     public InputHandler(MilitaryCadence cadence) {
-
-
+        IDontKnowCommand = new Command[0];
+        InArmyCommand = new Command[1];
+        EverywhereCommand = new Command[2]; 
     }
 
     public int playCadence(int num) {
+        if(num == 0)
+        {
+            return 0; 
+        }
+        if(num == 1) {
+            return 1;
+        }
+        if(num == 2) {
+            return 2;
+        }
+        else {
+            return -1;
+        }
         
     }
 
